68f77da889dbc878661f65e71b49921193966c3e
cathylyirang <cathylyirang@gmail.com>
2024-09-22 20:39:44 -0400

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,38 @@
-from hello import exp_fun
+"""
+Test goes here
 
-def test_exp_fun():
-  assert exp_fun(2, 2) == 4
+"""
+
+from hello import (
+    get_describe,
+    col_summ_stats,
+    generate_visualization,
+    save_to_md,
+)
+
+
+def test_get_describe():
+    test_describe = get_describe()
+    assert test_describe.shape == (8, 7)
+
+
+def test_col_summ_stats():
+    col = "ShareWomen"
+    womenstem_data_describe = get_describe()
+    summ_stats_dictionary = col_summ_stats(col)
+    assert (
+        womenstem_data_describe.loc["mean", "ShareWomen"]
+        == summ_stats_dictionary["mean"]
+    )
+    assert (
+        womenstem_data_describe.loc["50%", "ShareWomen"]
+        == summ_stats_dictionary["median"]
+    )
+    assert (
+        womenstem_data_describe.loc["std", "ShareWomen"] == summ_stats_dictionary["std"]
+    )
+
+
+if __name__ == "__main__":
+    test_get_describe()
+    test_col_summ_stats()
